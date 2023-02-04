c39deb351b3d2cebb497c7ca016298741ec184a9
ethanjones <egj77508@uga.edu>
2023-02-04 09:18:00 -0500

    Added person.py and coordinates.py and implemented them

---
+++ whereWeStoppin/objects/person.py
@@ -0,0 +1,8 @@
+from coordinates import Coordinates
+from room import Room
+
+
+class Person:
+    location = Coordinates(0, 0)
+    preferences = []
+    room = Room()
--- whereWeStoppin/objects/room.py
+++ whereWeStoppin/objects/room.py
@@ -2,3 +2,8 @@ class Room:
     id = ""
     users = []
     eventType = ""
+
+    #def generateid(self):
+
+        # id generation function
+#    def __init__(self):
